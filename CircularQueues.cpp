5c0a8ac48108c5e16cd7a5d4bd66aeb4eec7a9e8
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-06-10 11:05:53 +0700

    membuat fungsi remove dan mengecek antrian kosong

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -7,32 +7,47 @@ class Queues
     int queue_array[5];
 
 public:
-Queues(){
-    FRONT = -1;
-    REAR = -1;
-}
-void insert(){
-   int num;
-    cout << "enter a number :";
-    cin >> num;
-    cout << endl; 
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+    void insert()
+    {
+        int num;
+        cout << "enter a number :";
+        cin >> num;
+        cout << endl;
 
-    //mengecek antrian penuh
-    if ((FRONT == 0 && REAR== max -1) ||(FRONT == REAR + 1)){
-        cout << "\nQueue overflow\n";
-        return;
+        // mengecek antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+        // cek antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT == 0;
+            FRONT == 0;
+        }
+        else
+        {
+            // jika REAR berada di posisi terakhir array, kembali ke awal
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
-    // cek antrian kosong
-    if (FRONT == -1){
-        FRONT == 0;
-        FRONT == 0;
-    }
-    else {
-        //jika REAR berada di posisi terakhir array, kembali ke awal
-        if (REAR == max -1)
-        REAR = 0;
-        else 
-        REAR = REAR + 1;
-    }
-}
-};
+    void remove()
+    {
+        // cek antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is:" << queue_array[FRONT] << "\n";
+        
